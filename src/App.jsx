f0f19d195c608fe7ef9662a9bfafa154afe222b9
Hamza <hamza.afzalkhan9@gmail.com>
2023-12-23 22:43:06 +0500

    add personal info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Hamza Afzal",
+  title: "Software Developer",
+  email: "hamza_afzall@outlook.com",
+  gitHub: "introvertNerd00",
+  instagram: "h.a.m.z.a_____k.h.a.n/",
+  linkedIn: "hamzaafzall/",
   medium: "",
   twitter: "microsoft",
-  youTube: "Code",
+  youTube: "BackStory",
 };
 
 const primaryColor = "#4E567E";
